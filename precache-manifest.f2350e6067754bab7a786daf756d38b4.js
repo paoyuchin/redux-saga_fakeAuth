cad23e839af2d8613937d9cfd3a3bb610fe80373
yuchin <yuchinpao@liontravel.com>
2019-08-09 17:41:43 +0800

    Updates

---
+++ precache-manifest.f2350e6067754bab7a786daf756d38b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9ae5a1717c0455e855cf9935787ea0",
+    "url": "/redux-saga_fakeAuth/index.html"
+  },
+  {
+    "revision": "fda98dc9f23ef2c039bb",
+    "url": "/redux-saga_fakeAuth/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f72d9c14d488fbde6979",
+    "url": "/redux-saga_fakeAuth/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fda98dc9f23ef2c039bb",
+    "url": "/redux-saga_fakeAuth/static/js/2.c2e78dae.chunk.js"
+  },
+  {
+    "revision": "f72d9c14d488fbde6979",
+    "url": "/redux-saga_fakeAuth/static/js/main.58138073.chunk.js"
+  },
+  {
+    "revision": "e5930a95b09cf276e52e",
+    "url": "/redux-saga_fakeAuth/static/js/runtime~main.9343e8c8.js"
+  }
+]);
